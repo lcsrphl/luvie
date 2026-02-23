954b86580eb2f7f6e9cc015254a566173fc991cf
lcsrphl <lcsrphl@gmail.com>
2026-02-23 13:51:25 -0300

    Atualizar o index.js

--- functions/index.js
+++ functions/index.js
@@ -57,7 +57,7 @@ app.post("/createCheckout", async (req, res) => {
           items,
           // seu “pedido interno”
           external_reference: doc.id,
-          notification_url: `${process.env.PUBLIC_FUNCTIONS_BASE_URL}/webhookMercadoPago`,
+          notification_url: "https://us-central1-luvie-app-2026.cloudfunctions.net/api/webhookMercadoPago",
           metadata: {
             pedidoId: doc.id,
             token
