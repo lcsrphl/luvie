e2436a523717ee5f6bd4068498aeaf6cea24e1a3
lcsrphl <lcsrphl@gmail.com>
2026-02-23 13:54:18 -0300

    Atualizar o app.js

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 // ✅ configs globais (front)
 window.__MP_PUBLIC_KEY__ = "TEST-51eb8f56-5597-4c1d-be93-3307bdbcf463"; 
 // depois que você criar Cloud Functions, você vai colocar aqui a URL da API:
-window.__API_BASE_URL__ = ""; // depois: https://us-central1-luvie-app-2026.cloudfunctions.net/api
+window.__API_BASE_URL__ = "https://us-central1-luvie-app-2026.cloudfunctions.net/api";
 
 
 import { addRoute, startRouter } from "./router.js";
