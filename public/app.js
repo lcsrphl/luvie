405c1adcff9668e375048bae1ae8b196dd0f7a77
lcsrphl <lcsrphl@gmail.com>
2026-02-25 14:43:49 -0300

    Atualizar o app.js

--- public/app.js
+++ public/app.js
@@ -1,18 +1,28 @@
 // ✅ configs globais (front)
-window.__MP_PUBLIC_KEY__ = "TEST-51eb8f56-5597-4c1d-be93-3307bdbcf463"; 
-// depois que você criar Cloud Functions, você vai colocar aqui a URL da API:
+window.__MP_PUBLIC_KEY__ = "TEST-51eb8f56-5597-4c1d-be93-3307bdbcf463";
 window.__API_BASE_URL__ = "https://us-central1-luvie-app-2026.cloudfunctions.net/api";
 
-
 import { addRoute, startRouter } from "./router.js";
+
 import { renderHome } from "./views/home.js";
 import { renderNewProduct } from "./views/new-product.js";
 import { renderCustomers } from "./views/customers.js";
 import { renderCheckout } from "./views/checkout.js";
-addRoute("/checkout", renderCheckout);
 
+// ✅ NOVAS TELAS
+import { renderOrders } from "./views/orders.js";
+import { renderOrderDetails } from "./views/order-details.js";
+import { renderTracking } from "./views/tracking.js";
+
+// rotas existentes
 addRoute("/", renderHome);
 addRoute("/novo-produto", renderNewProduct);
 addRoute("/clientes", renderCustomers);
+addRoute("/checkout", renderCheckout);
+
+// ✅ rotas novas
+addRoute("/pedidos", renderOrders);
+addRoute("/pedido", renderOrderDetails);     // usa /pedido?id=XXXX
+addRoute("/acompanhar", renderTracking);     // usa /acompanhar?t=TOKEN
 
 startRouter(document.getElementById("app"));
