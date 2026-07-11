4208bd4772905fd7db23791e1e9ff304548cd9a8
lcsrphl <lcsrphl@gmail.com>
2026-07-11 15:48:13 -0300

    Atualizar o app.js

--- public/app.js
+++ public/app.js
@@ -1,4 +1,4 @@
-// ✅ configs globais (front)
+// ✅ configs globais (fronti)
 window.__MP_PUBLIC_KEY__ = "TEST-51eb8f56-5597-4c1d-be93-3307bdbcf463";
 window.__API_BASE_URL__ = "https://us-central1-luvie-app-2026.cloudfunctions.net/api";
 
